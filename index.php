fc08a63124038f49e9ab83675f9de98626f3d14f
Manvendra Singh <manvendrask2@gmail.com>
2017-12-07 20:41:40 +0530

    Removed flexor pages.

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('./app/common/header.php');
-require_once('./app/home/content.php');
-require_once('./app/common/hiring.php');
-require_once('./app/common/clients.php');
-require_once('./app/common/footer.php');
-?>
